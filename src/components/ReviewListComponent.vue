68bf3e32fc47985c06e734cee48b6da1b9860586
福原佑介 <yusuke@fukuharayuusukenoMacBook-Pro.local>
2021-02-26 22:07:22 +0900

    fix

--- src/components/ReviewListComponent.vue
+++ src/components/ReviewListComponent.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="review-list" v-cloak>
+  <div id="review-list" >
     <div class="row" style="margin: auto 5px;">
       <div class="col-0  col-sm-1   col-md-2  col-xl-3"></div>
       <div class="col-12 col-sm-10 col-md-8 col-xl-6 my-auto">
         <!-- place name -->
-        <h2 class="text-center h1" style="margin: 20px auto; word-break: normal;">{{ name }}</h2>
+        <h2 class="text-center h1" style="margin: 20px auto; word-break: normal;" v-cloak>{{ name }}</h2>
 
         <!-- rating -->
         <div class="row" style="margin: 1rem auto">
           <!-- start rating -->
           <div class="col-4 col-sm-3 col-md-3 col-lg-3 col-xl-3 my-auto">
-            <div style="text-align:center; font-size: 2rem;" class="my-auto">{{ this.aveRating }}</div>
-            <star-rating v-model="aveRating" :show-rating="false" :increment="0.1" :read-only="true" :star-size="this.starSize" style="margin: 5px auto; width: 100%; justify-content: center;" v-cloak></star-rating>
+            <div style="text-align:center; font-size: 2rem;" class="my-auto">{{ aveRating }}</div>
+            <star-rating v-model="aveRating" :show-rating="false" :increment="0.1" :read-only="true" :star-size="starSize" style="margin: 5px auto; width: 100%; justify-content: center;" v-cloak></star-rating>
           </div>
           <!-- rating bar -->
           <div class="col-8 col-sm-9 col-md-9 col-lg-9 col-xl-9">
